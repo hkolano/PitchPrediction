78b18f4835951057b7a2b7bd9c17e75f24d7568e
Hannah Kolano <hkolano@comcast.net>
2022-06-13 12:17:09 -0700

    started 1d data training

--- src/waveGenerator.jl
+++ src/waveGenerator.jl
@@ -1,12 +1,35 @@
-using CSV, Tables
+using CSV, Tables, Plots
 
-sample_freq = 20. # Hz
-wave_freq = 1. # Hz
+sample_freq = 100. # Hz
 time_span = 2
-amp = rand(1)
+t_seq = [n for n=0:1/sample_freq:2]
 
-t_seq = [0:1/sample_freq:2]
-wave = amp.*sin.(t_seq*wave_freq)
+wave(a, f, t) = a.*sin.(t.*f)
+
+function gen_simple_wave(t::Vector{Float64})
+    wave_freq = rand(1)*10*2π
+    amp = rand(1)
+    return wave(amp, wave_freq, t)
+end
+
+function generate_wave_file(i, t=t_seq, time=time_span)
+    # Initialize data array
+    num_rows = length(t)
+    data = Array{Float64}(undef, num_rows, 2)
+
+    # First column (time): 
+    data[:,1] = t 
+    # Second column (wave):
+    data[:,2] = gen_simple_wave(t)
+    labels = ["time", "wave"]
+
+    tab = Tables.table(data)
+    CSV.write("data/waves/simplewave$(i).csv", tab, header=labels)
+end
+
+for i = 1:10
+    generate_wave_file(i)
+end
 
 # print(wave)
 
