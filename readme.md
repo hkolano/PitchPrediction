518c3def3e3b9e3332588d8c1a47d374005d8cc3
Hannah Kolano <hkolano@comcast.net>
2022-06-15 14:37:24 -0700

    PD controller follows desired velocity on toy problem

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Pitch Prediction
+
+
+
+## Running Notes
+
+6/15/22:
+If you want to update changes to an imported module, it's better to call the entire thing: MyMod.foo() instead of just foo(). "Include" just copies the current version of the code and doesn't update it when it gets changed. 
--- src/toy-problem/PDCtlr.jl
+++ src/toy-problem/PDCtlr.jl
@@ -1,93 +1,87 @@
 module PDCtlr 
+# export ctlr_setup
 
 using RigidBodyDynamics
 
 Kp = 50.
 Kd = 2.0
-torque_lims = (joint1=5.0, joint2=5.0)
-vel_error_cache = (joint1=[0.0], joint2=[0.0])
+torque_lims = [0.0, 5.0, 5.0]
+vel_error_cache = [0.0, 0.0, 0.0]
 # ------------------------------------------------------------------------
 #                          SETUP FUNCTIONS
 # ------------------------------------------------------------------------
-    function ctlr_setup(mechanism, state; time_step=1e-2,)
-        set_ctlr_time_step(time_step)
-        resettimestep()
-        set_mechanism_to_control(mechanism)
-        set_first_state(state)
-    end
+function ctlr_setup(mechanism::Mechanism{Float64}, state::MechanismState; time_step=1e-2)
+    global dt = time_step
+    resettimestep()
+    set_first_state(state)
+    free_joint, joint1, joint2 = joints(mechanism)
+    global joint_vec = [free_joint, joint1, joint2]
+end
 
-    function set_ctlr_time_step(time_step)
-        global dt = time_step
-    end
+function resettimestep()
+    global time_step_ctr = 0
+end
 
-    function resettimestep()
-        global time_step_ctr = 0
-    end
-
-    function set_mechanism_to_control(mechanism)
-        global ctlr_mechanism = mechanism
-        global base_joint, shoulder_joint, elbow_joint, wrist_joint = joints(ctlr_mechanism)
-    end
-
-    function set_first_state(state)
-        global ctlr_first_state = state 
-        global des_vel = similar(velocity(ctlr_first_state))
-        global prev_vels = similar(des_vel)
-    end
+function set_first_state(state)
+    ctlr_first_state = state 
+    global des_vel = similar(velocity(ctlr_first_state))
+    global prev_vels = similar(des_vel)
+end
 
 # ------------------------------------------------------------------------
 #                          UTILITY FUNCTIONS
 # ------------------------------------------------------------------------
-    function impose_torque_limit!(torque, limit)
-        if torque > limit
-            torque = limit
-        elseif torque < -limit
-            torque = -limit
-        end
+function impose_torque_limit!(torque, limit)
+    if torque > limit
+        torque = limit
+    elseif torque < -limit
+        torque = -limit
     end
+end
 
 # ------------------------------------------------------------------------
 #                              CONTROLLER
 # ------------------------------------------------------------------------
 
-function simple_control!(torques::AbstractVector, t, state_alpha::MechanismState, hydro_wrenches::Dict{BodyID, Wrench{Float64}})
+function pd_control!(torques::AbstractVector, t, state::MechanismState; time_step_ctr=time_step_ctr, joint_vec=joint_vec)
     # If it's the first time called in the Runge-Kutta, update the control torque
+    (j1, j2, j3) = joint_vec
     if rem(time_step_ctr, 4) == 0
         # println("New control goes here!!!")
 
-        # Get the desired velocity values for each joint
+        torques[velocity_range(state, j1)] .= -.1 .* velocity(state, j1)
+        # torques[velocity_range(state, j2)] .= -.1 .* velocity(state, j2)
+        # torques[velocity_range(state, j3)] .= -.1 .* velocity(state, j3)
+
         get_des_vel!(des_vel, t)
 
-        tau_base = MyCtlr.PD_ctlr(torques[velocity_range(state_alpha, base_joint)][1], t, velocity(state_alpha, base_joint), des_vel[base_joint], 1)
-        tau_shoulder = MyCtlr.PD_ctlr(torques[velocity_range(state_alpha, shoulder_joint)][1], t, velocity(state_alpha, shoulder_joint), des_vel[shoulder_joint], 2)
-        tau_elbow = MyCtlr.PD_ctlr(torques[velocity_range(state_alpha, elbow_joint)][1], t, velocity(state_alpha, elbow_joint), des_vel[elbow_joint], 3)
-        tau_wrist = MyCtlr.PD_ctlr(torques[velocity_range(state_alpha, wrist_joint)][1], t, velocity(state_alpha, wrist_joint), des_vel[wrist_joint], 4)
+        for idx in 2:3
+            # println("at idx $(idx)")
+            torques[idx] = PD_ctlr(torques[idx][1], t, velocity(state, joint_vec[idx]), des_vel[idx], idx) - .1 .*velocity(state, joint_vec[idx])
+        end
 
-        torques[velocity_range(state_alpha, base_joint)] .= tau_base
-        torques[velocity_range(state_alpha, shoulder_joint)] .= tau_shoulder #tau[shoulder_joint][1]
-        torques[velocity_range(state_alpha, elbow_joint)] .= tau_elbow #0.0 #tau[elbow_joint][1]
-        torques[velocity_range(state_alpha, wrist_joint)] .= tau_wrist #tau[wrist_joint][1]
     end
 
     # push!(ctlr_taus, tau_base)
     # push!(times, t)
 
-    global time_step_ctr += 1
+    time_step_ctr += 1
     # println("Base joint vel errors: ", vel_error_cache.base_joint)
 end;
 
-function get_des_vel!(des_vel, t)
+function get_des_vel!(des_vel, t; joint_vec=joint_vec)
     # TODO: implement a simple pt a to pt b trajectory generation
-    des_vel[base_joint][1] = 1.0
-    des_vel[shoulder_joint][1] = 0.0
-    des_vel[elbow_joint][1] = 0.0 #vdot[elbow_joint][1]
-    des_vel[wrist_joint][1] = 0.00
+    (~, j2, j3) = joint_vec
+    des_vel[j2][1] = 1.0
+    des_vel[j3][1] = 0.0
 end;
     
-function PD_ctlr(torque, t, vel_act, des_vel, j_idx)
+function PD_ctlr(torque, t, vel_act, des_vel, j_idx; dt=dt)
     vel_error = vel_act[1] - des_vel[1]
-    d_vel_error = (vel_error - last(vel_error_cache[j_idx]))/dt
+    d_vel_error = (vel_error - vel_error_cache[j_idx])/dt
+    # println("D_Velocity error on idx$(j_idx): $(d_vel_error)")
     d_tau = -Kp*vel_error - Kd*d_vel_error
+    # println("Ideal tau: $(d_tau)")
 
     # Can only change torque a small amount per time step
     if d_tau < -.05
@@ -100,8 +94,8 @@ function PD_ctlr(torque, t, vel_act, des_vel, j_idx)
     new_tau = torque .+ d_tau
     impose_torque_limit!(new_tau, torque_lims[j_idx])
 
-    # store velocity error term
-    push!(vel_error_cache[j_idx], vel_error)
+    # # store velocity error term
+    vel_error_cache[j_idx]=vel_error
 
     return new_tau
 end
