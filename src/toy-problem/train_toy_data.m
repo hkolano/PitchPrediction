fd85b9d7a959f6a18a374c266ec3e184dd029da5
Hannah Kolano <hkolano@comcast.net>
2022-07-05 12:44:51 -0700

    moved data import to separate file

--- src/toy-problem/train_toy_data.m
+++ src/toy-problem/train_toy_data.m
@@ -1,23 +1,7 @@
-function train_toy_data(folder)
-    fds = fileDatastore(folder,"ReadFcn",@read_to_array);
+function train_toy_data()
+    data = import_toy_training_data("data/toy-data");
 
-    data = readall(fds);
-%     figure 
-    plot(data{2}(5,:))
-%     hold on
-%     data = smooth_velocities(data);
-%     plot(data{2}(5,:))
 
-%     % Preview the data
-%     % figure
-%     % tiledlayout(2,2)
-%     % for i = 1:4
-%     %     nexttile
-%     %     stackedplot(data{i})
-%     % 
-%     %     xlabel("Time Step")
-%     % end
-% 
     % Divide into train and test data
     numObservations = numel(data);
     idxTrain = 1:floor(0.9*numObservations);
@@ -58,19 +42,6 @@ function train_toy_data(folder)
 
 %     net = trainNetwork(XTrain,TTrain,layers,options);
 
-    function data = read_to_array(input)
-        data = readtable(input);
-        data = table2array(data)';
-    end
-
-    function data = smooth_velocities(data)
-        window = 21;
-        for i = 1:numel(data)
-            data{i}(5,:) = movmean(data{i}(5,:), window);
-            data{i}(6,:) = movmean(data{i}(6,:), window);
-            data{i}(7,:) = movmean(data{i}(7,:), window);
-        end
-    end
 
 %     outputFile = fullfile("data/networks/toy-nets", 'netv1.mat');
 %     save(outputFile, 'net');
