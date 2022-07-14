e85988aacc0686f15a9b5a7b94481012de989822
Hannah Kolano <hkolano@comcast.net>
2022-07-13 18:34:31 -0700

    trained v3

--- src/toy-problem/retrain_toy_data.m
+++ src/toy-problem/retrain_toy_data.m
@@ -29,7 +29,7 @@ error = validate(net)
 error_vec = [error];
 % pred = toy_forecast(net, XTest{1}, 100, 25, p, true);
 
-for retrain_idx = 1:20
+for retrain_idx = 1:1000
     
     for it_num = 1:20
         traj_idx = randi(size(XTrain, 2));
@@ -57,7 +57,7 @@ for retrain_idx = 1:20
     
     net = trainNetwork(preds, g_truth, layerGraph(net), retrain_options);
     
-    if rem(retrain_idx, 5) == 0
+    if rem(retrain_idx, 25) == 0
         error = validate(net)
         disp(retrain_idx)
         error_vec = [error_vec error];
@@ -68,8 +68,8 @@ end
 % pred = toy_forecast(net, XTest{1}, 100, 25, p, true);
 
 % Save the output
-outputFile = fullfile("data/networks/toy-nets", 'retrained_071322_v3.mat');
-save(outputFile, 'net');
+% outputFile = fullfile("data/networks/toy-nets", 'retrained_071322_v4.mat');
+% save(outputFile, 'net');
 
 function error = validate_net(net, X_test, idxs, ns, k, p)
     error = 0;
