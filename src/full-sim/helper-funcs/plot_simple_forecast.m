05ff574f03fa12a4d8d09c82ced0f297b4929bf5
Hannah Kolano <hkolano@comcast.net>
2022-08-15 17:24:39 -0700

    pitch plotting

---
+++ src/full-sim/helper-funcs/plot_simple_forecast.m
@@ -0,0 +1,13 @@
+function plot_simple_forecast(net, data, n, k)
+
+% Make prediction
+resetState(net)
+[new_net, Z] = predictAndUpdateState(net, data(:,1:n), "ExecutionEnvironment","auto");
+
+close all; 
+plot(data(14,:), 'Color', '#DDCC77', 'LineWidth', 1)
+hold on
+plot(Z(1,:), '--', 'Color', '#0072B2', 'LineWidth', 2)
+plot(n+1:n+k, Z(:,end), '--', 'Color', '#56B4E9', 'LineWidth', 2)
+
+end
