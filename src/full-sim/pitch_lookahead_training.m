c58e6436004590287c148cd787a1878b7cf771cc
Hannah Kolano <hkolano@comcast.net>
2022-08-17 11:11:37 -0700

    updated pitch_lookahead_training to deal with short sequences

--- src/full-sim/pitch_lookahead_training.m
+++ src/full-sim/pitch_lookahead_training.m
@@ -1,26 +1,31 @@
-load('data/full-data-matlab/FullData_NoVehXYZ_noB_noWaypoints_081522.mat')  
+load('data/full-data-matlab/FullData_NoVehXYZ_noB_noWaypoints_081222.mat')  
 
-k = 40;
+k = 150;
 numChannels = size(XTrain{1}, 1);
 pitch_idx = 14;
 layers = setup_lookahead_rnn(numChannels, k, 1024);
 
 for n = 1:numel(XTrain)
     resp = [];
-    for t = 1:length(XTrain{n})-k
-        resp = [resp, XTrain{n}(pitch_idx, t+1:t+k)'];
+%     disp(length(XTrain{n}))
+    if length(XTrain{n}) > 1.1*k
+        for t = 1:length(XTrain{n})-k
+            resp = [resp, XTrain{n}(pitch_idx, t+1:t+k)'];
+        end
+        Resp_Train{n} = resp;
+        Inputs_Train{n} = XTrain{n}(:,1:end-k);
     end
-    Resp_Train{n} = resp;
-    XTrain{n} = XTrain{n}(:,1:end-k);
 end
 
 for n = 1:numel(XTest)
     resp = [];
-    for t = 1:length(XTest{n})-k
-        resp = [resp, XTest{n}(pitch_idx, t+1:t+k)'];
+    if length(XTest{n}) > 1.1*k
+        for t = 1:length(XTest{n})-k
+            resp = [resp, XTest{n}(pitch_idx, t+1:t+k)'];
+        end
+        Resp_Test{n} = resp;
+        Inputs_Test{n} = XTest{n}(:,1:end-k);
     end
-    Resp_Test{n} = resp;
-    XTest{n} = XTest{n}(:,1:end-k);
 end
 
 %     MaxEpochs=5, ...
@@ -30,11 +35,10 @@ init_options = trainingOptions("adam", ...
     SequencePaddingDirection="right", ...
     Plots="training-progress", ...
     Shuffle='never', ...
-    ValidationData={XTest, Resp_Test}, ...
+    ValidationData={Inputs_Test, Resp_Test}, ...
     ValidationPatience = 5, ...
-    ValidationFrequency = 50, ...
-    OutputNetwork = 'best-validation-loss');
-[net, info] = trainNetwork(XTrain,Resp_Train,layers,init_options);
+    ValidationFrequency = 50);
+[net, info] = trainNetwork(Inputs_Train,Resp_Train,layers,init_options);
 %     
 outputFile = fullfile("data/networks/full-nets/Z_3_nets", strcat('Z_3_', string(k), 'steps.mat'));
 save(outputFile, 'net', 'info');
