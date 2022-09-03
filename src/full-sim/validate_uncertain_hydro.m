b0174818ce86044593f0edeec9f8e3701ac0caa1
Hannah Kolano <hkolano@comcast.net>
2022-09-02 17:36:05 -0700

    update_uncertain_hydro runs more autonomously

--- src/full-sim/validate_uncertain_hydro.m
+++ src/full-sim/validate_uncertain_hydro.m
@@ -1,27 +1,42 @@
-load("data\full-sim-with-hydro\single-model-1percent\arm-added-mass\model1_data_18chan.mat");
+
 load("data\networks\full-nets\simple_network_unit_testing\unittesting_256units_take1_learningdrop.mat");
 
 k = 25;
+param_names = ["arm-added-mass", "arm-linear-drag", "vehicle-added-mass", "vehicle-linear-drag", "vehicle-quadratic-drag"];
 
-for n = 1:numel(XData)
-    resp = zeros(k, size(XData{n}, 2)-k);
-    if size(XData{n}, 2) > 1.1*k
-        for t = 1:size(XData{n}, 2)-k
-            resp(:,t) = XData{n}(pitch_idx, t+1:t+k)';
+rmses = zeros(5, 3);
+
+for param_id = 1:length(param_names)
+
+    param = param_names(param_id);
+
+    for model_num = 1:3
+    
+        load(strcat("data\full-sim-with-hydro\single-model-50percent\", param, "\model", string(model_num), "_data_18chan.mat"));
+        
+        for n = 1:numel(XData)
+            resp = zeros(k, size(XData{n}, 2)-k);
+            if size(XData{n}, 2) > 1.1*k
+                for t = 1:size(XData{n}, 2)-k
+                    resp(:,t) = XData{n}(pitch_idx, t+1:t+k)';
+                end
+                Resp{n} = resp;
+                Inputs{n} = [XData{n}(:,1:end-k); repmat(0.02, 1, size(XData{n},2)-k)];
+            end
         end
-        Resp{n} = resp;
-        Inputs{n} = [XData{n}(:,1:end-k); repmat(0.02, 1, size(XData{n},2)-k)];
+        
+        resetState(net)
+        [net, Z] = predictAndUpdateState(net, Inputs, "ExecutionEnvironment","auto");
+        
+        RMSEs = zeros(1, numel(XData));
+        
+        for n = 1:numel(XData)
+            RMSEs(n) = sqrt(immse(single(Resp{n}), Z{n})); 
+        end
+    
+        rmses(param_id, model_num) = mean(RMSEs);
     end
 end
 
-resetState(net)
-[net, Z] = predictAndUpdateState(net, Inputs, "ExecutionEnvironment","auto");
+disp(rmses)
 
-RMSEs = zeros(1, numel(XData));
-
-for n = 1:numel(XData)
-    RMSEs(n) = sqrt(immse(single(Resp{n}), Z{n})); 
-end
-
-figure
-plot(RMSEs)
