dc843db3a6a79b77c86a3c6b7a4755600319972a
Hannah Kolano <hkolano@comcast.net>
2022-11-08 16:10:13 -0800

    Started recursive ablation framework

---
+++ src/full-sim/ablation_study_recursive.m
@@ -0,0 +1,109 @@
+load('data/channel_dict.mat')
+chan_idxs = rmfield(chan_idxs, 'pitch');
+chan_idxs = rmfield(chan_idxs, 'dt');
+fn = fieldnames(chan_idxs);
+
+% Load in the data
+load("data/full-data-matlab/FullData_081022.mat")
+
+%%
+
+% Make a list of all channel indices
+all_idxs = 1:1:41;
+
+% Initialize constants
+k = 25;
+numUnits = 128;
+pitch_idx = 23;
+
+% Set up vectors to store loss values
+level_losses = [];
+all_losses = [];
+all_RMSEs = [];
+
+% Iterate over all remaining features
+for feat_idx = 1:length(fn)
+    % What feature name are we removing?
+    feat_name = fn{feat_idx}
+    % What channels does that feature group contain?
+    feat_chan_idxs = chan_idxs.(feat_name)
+
+    % Get the indices of the remaining channels, sans the feature group in
+    % question
+    rem_idxs = all_idxs(~ismember(all_idxs, feat_chan_idxs));
+
+    % Cut the group's data out of the dataset
+    clear Resp_Train Inputs_Train Resp_Test Inputs_Test
+    [Inputs_Train, Resp_Train] = split_data(XTrain, pitch_idx, k, rem_idxs);
+    [Inputs_Test, Resp_Test] = split_data(XTest, pitch_idx, k, rem_idxs);
+
+    % How many feature channels remain?
+    numChannels = size(Inputs_Train{1}, 1)
+    
+    % Initialize the neural network
+    layers = setup_lookahead_rnn(numChannels, k, numUnits);
+    init_options = define_new_opts(Inputs_Test, Resp_Test);
+    subgroup_losses = [];
+    subgroup_RMSEs = [];
+
+   for take_n = 1:2 % 3
+        [net, info] = trainNetwork(Inputs_Train,Resp_Train,layers,init_options);
+        
+        subgroup_losses = [subgroup_losses, info.FinalValidationLoss];
+        subgroup_RMSEs = [subgroup_RMSEs, info.FinalValidationRMSE];
+        %     
+        outputFile = strcat("data/networks/icra-redo-nets/abl_rnd1/no_", feat_name, '_take_', string(take_n), '.mat');
+        save(outputFile, 'net', 'info');
+   end
+
+   level_losses = [level_losses, mean(subgroup_losses)];
+%    all_losses = [all_losses; subgroup_losses];
+%    all_RMSEs = [all_RMSEs; subgroup_RMSEs];
+end
+
+disp(level_losses)
+[min_val, min_idx] = min(level_losses);
+smallest_impact_feat = fn{min_idx}
+% disp("Final Losses:")
+% disp(all_losses)
+% disp("Final RMSEs:")
+% disp(all_RMSEs)
+% beep
+
+% end
+
+% ------------------------------------------------------------------------
+%                                Functions
+% ------------------------------------------------------------------------
+
+function [inputs, outputs] = split_data(data, pitch_idx, k, rem_feat_idxs)
+    for n = 1:numel(data)
+        resp = zeros(k, size(data{n}, 2)-k);
+        if size(data{n}, 2) > 1.1*k
+            for t = 1:size(data{n}, 2)-k
+                resp(:,t) = data{n}(pitch_idx, t+1:t+k)';
+            end
+            outputs{n} = resp;
+            inputs{n} = data{n}(rem_feat_idxs,1:end-k);
+        end
+    end
+end
+
+function init_options = define_new_opts(val_inputs, val_outputs)
+    init_options = trainingOptions("adam", ...
+        InitialLearnRate=0.001,...
+        LearnRateDropPeriod=5, ...
+        LearnRateSchedule='piecewise', ...
+        LearnRateDropFactor=.8, ...
+        MaxEpochs = 1, ...
+        MiniBatchSize=16, ...
+        SequencePaddingDirection="right", ...
+        Plots="training-progress", ...
+        Shuffle='every-epoch', ...
+        ValidationData={val_inputs, val_outputs}, ...
+        ValidationFrequency = 60, ...
+        OutputNetwork='best-validation-loss');
+end
+
+
+
