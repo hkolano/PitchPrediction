74b6f98a671f1e3825ddda228c7befe4ff65c593
Hannah Kolano <hkolano@comcast.net>
2022-12-06 11:30:10 -0800

    validation set creates 10hz pts and 50hz pts

--- src/full-sim/helper-funcs/generate_validation_set.m
+++ src/full-sim/helper-funcs/generate_validation_set.m
@@ -1,12 +1,11 @@
-% Define a validation set
-% load('data/full-data-matlab/channel_subgroups/no_goal_poses/no_manip_vels/no_goal_vels/data_without_xyz_poses.mat')
-load('data/full-sim-data-110822/FullData_10Hz.mat')
-k = 4;
+%{ 
+Defines a validation set to use for the stretch study. Saves 250 indicies
+and "start points", for 10hz and for 50hz.
 
-% XTest = XTest_10hz(1:end-5);
-% XTrain = XTrain_10hz(1:end-35);
-% TTest = TTest_10hz(1:end-5);
-% TTrain = TTrain_10hz(1:end-35);
+Last modified 12/6/22
+%}
+load('data/full-sim-data-110822/FullData_10Hz.mat')
+k = 4; % minimum traj length in seconds
 
 XTest = XTest_10hz;
 [~,I] = sort(cellfun(@length,XTest));
@@ -14,12 +13,15 @@ XTest = XTest(I);
 XTest = XTest(3:end); % only use trajectories with lengths longer than 4s
 
 num_vals = 250;
-val_idxs = [];
-val_ns = [];
+val_idxs = zeros(1, num_vals);
+val_ns_10hz = zeros(1, num_vals);
+val_ns_50hz = zeros(1, num_vals);
+
 for i = 1:num_vals
     val_idxs(i) = randi(numel(XTest));
-    val_ns(i) = randi([2, size(XTest{val_idxs(i)}, 2)-10*k-1]);
+    val_ns_10hz(i) = randi([2, size(XTest{val_idxs(i)}, 2)-10*k-1]);
+    val_ns_50hz(i) = val_ns_10hz(i)*5;
 end
 
-outputFile = fullfile("data/full-sim-data-110822", 'val_set_10Hz.mat');
-save(outputFile, 'val_idxs', 'val_ns');
+outputFile = fullfile("data/full-sim-data-110822", 'val_set.mat');
+save(outputFile, 'val_idxs', 'val_ns_10hz', 'val_ns_50hz');
