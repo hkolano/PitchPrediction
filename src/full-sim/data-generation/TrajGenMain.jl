ecf72f79c4dded04c811ce5a8f70fa431e0f5656
Hannah Kolano <hkolano@comcast.net>
2023-01-24 17:07:51 -0800

    generate straight line in world space

--- src/full-sim/data-generation/TrajGenMain.jl
+++ src/full-sim/data-generation/TrajGenMain.jl
@@ -3,6 +3,8 @@ module TrajGen
 include("StructDefs.jl")
 using JLD
 using Random
+using StaticArrays, RigidBodyDynamics, Rotations
+
 
 # ----------------------------------------------------------
 #                         Definitions
@@ -10,12 +12,14 @@ using Random
 num_its=50
 num_actuated_dofs = 5
 num_trajectory_dofs = 4
+max_duration = 20.
 θa = atan(145.3, 40)
 joint_lims = [[-175*pi/180, 175*pi/180], [0, 200*pi/180], [0, 200*pi/180], [-165*pi/180, 165*pi/180], [0.0, 0.022]]
 # Velocity limits: 30 degrees/s for Joints E, D, C, 50 degrees/s for Joint B (from Reach Alpha Integration Manual V1.15 p8)
 θb = deg2rad(30)
 θc = deg2rad(50)
 vel_lims = [[-θb, θb], [-θb, θb], [-θb, θb], [-θc, θc], [-.003, .003]]
+max_linear_vel = .1 # 10 cm/s
 
 equil_pose = zeros(num_actuated_dofs)
 equil_pt = jointState(equil_pose, zeros(num_actuated_dofs))
@@ -29,33 +33,9 @@ mutable struct trajParams
     T::Float64      # Duration of the trajectory
 end
 
-# ----------------------------------------------------------
-#                     Point Generation
-# ----------------------------------------------------------
-""" 
-    gen_rand_feasible_point()
-
-Randomly generates a jointState, checking for joint and velocity limits.
-"""
-function gen_rand_feasible_point()
-    θs = Array{Float64}(undef,num_actuated_dofs)    
-    dθs = Array{Float64}(undef,num_actuated_dofs)    
-    for jt_idx in  1:num_actuated_dofs
-        θs[jt_idx] = rand(joint_lims[jt_idx][1]:.001:joint_lims[jt_idx][2])
-        dθs[jt_idx] = rand(vel_lims[jt_idx][1]:.001:vel_lims[jt_idx][2])
-    end
-    return jointState(θs, dθs)
-end
-
-"""
-    gen_rand_feasible_point_at_rest()
-
-Randomly generates a jointState where the velocity is 0. 
-"""
-function gen_rand_feasible_point_at_rest()
-    jS = gen_rand_feasible_point()
-    jS.dθs = [0.0 0.0 0.0 0.0 0.0]
-    return jS
+mutable struct worldSpaceWaypoints
+    start_pose
+    end_pose
 end
 
 # ----------------------------------------------------------
@@ -113,63 +93,109 @@ end
 # ----------------------------------------------------------
 #             Waypoint Generation (World Space)
 # ----------------------------------------------------------
-function generate_random_pose()
-    rand_scalings = [rand(.1: .01: 2) for i in 1:3]
+function generate_random_pose(mech)
+    base_frame = root_frame(mech)
+    cartesian_bound = 2
+    rand_scalings = [rand(.1: .01: cartesian_bound) for i in 1:3]
     rand_trans = Random.rand(Transform3D, base_frame, CartesianFrame3D("rand_frame"))
     new_translation = rand_trans.mat[1:3, 4] .* rand_scalings
-    mod_trans = Transform3D(base_frame, CartesianFrame3D("rand_frame"), rotation(rand_trans), SVector{3}(new_translation))
+    mod_trans = Transform3D(base_frame, CartesianFrame3D("rand_frame"), rotation(rand_trans), SVector{3, Float64}(new_translation))
     return mod_trans
 end
 
+function generate_path_from_current_pose(state)
+    base_frame = root_frame(state.mechanism)
+    jaw_frame = default_frame(bodies(state.mechanism)[end])
+    worldSpaceWaypoints(relative_transform(state, base_frame, jaw_frame), generate_random_pose(state.mechanism))
+end
+
+function get_T_at_s(wp::worldSpaceWaypoints, s::Float64)
+    p_start = translation(wp.start_pose) 
+    p_end = translation(wp.end_pose) 
+    R_start = rotation(wp.start_pose)
+    R_end = rotation(wp.end_pose)
+
+    p_at_s = p_start + s*(p_end - p_start)
+    R_at_s = R_start*exp(log(transpose(R_start)*R_end)*s)
+    T_at_s = Transform3D(wp.start_pose.from, wp.start_pose.to, R_at_s, p_at_s)
+end
+
+function get_Tdot_at_sdot(wp::worldSpaceWaypoints, s, sdot)
+    p_start = translation(wp.start_pose) 
+    p_end = translation(wp.end_pose) 
+    R_start = rotation(wp.start_pose)
+    R_end = rotation(wp.end_pose)
+
+    pdot = sdot*(p_end - p_start)
+    logRTR = log(transpose(R_start)*R_end)
+    Rdot = Rotations.RotMatrix(R_start*exp(logRTR*s)*logRTR*sdot)
+    # println(R_start*exp(logRTR*s)*logRTR*sdot)
+    T_at_s = Transform3D(wp.start_pose.from, wp.start_pose.to, Rdot, pdot)
+end
+
 # ----------------------------------------------------------
-#                  Trajectory Generation
+#               Trajectory Generation (World Space)
 # ----------------------------------------------------------
-""" 
-    get_coeffs(pts::Waypoints, T, idx)
 
-Given a set of waypoints and a time scaling, determine the time scaling coefficients
-for a quintic trajectory. 
-"""
-function get_coeffs(pts::Waypoints, T, idx)
-    # λ1 = dθ1/(θ2-θ1)
-    λ1 = pts.start.dθs[idx]/(pts.goal.θs[idx]-pts.start.θs[idx])
-    # λ2 = dθ2/(θ2-θ1)
-    λ2 = pts.goal.dθs[idx]/(pts.goal.θs[idx]-pts.start.θs[idx])
-
-    a0 = 0
-    a1 = λ1
-    a2 = 0
-    a3 = -2(3T*λ1+2T*λ2-5)/(T^3)
-    a4 = (8T*λ1 + 7T*λ2-15)/(T^4)
-    a5 = -3(T*λ1+T*λ2-2)/(T^5)
-    return [a0, a1, a2, a3, a4, a5]
+function get_coeffs(T)
+    a3 = 10/(T^3)
+    a4 = -15/(T^4)
+    a5 = 6/(T^5)
+    return [0, 0, 0, a3, a4, a5]
 end
 
-function pos_scale_at_t(a, t)
-    s = a[1] + a[2]*t + a[3]*t^2 + a[4]*t^3 + a[5]*t^4 + a[6]*t^5
-end
+s(t, a) = a[1] + a[2]*t + a[3]*t^2 + a[4]*t^3 + a[5]*t^4 + a[6]*t^5
+ds(t, a) = a[2] + 2a[3]*t + 3a[4]*t^2 + 4a[5]*t^3 + 5a[6]*t^4
+dds(t, a) = 2a[3] + 6a[4]*t + 12a[5]*t^2 + 20a[6]*t^3
 
-function vel_scale_at_t(a, t)
-    ds = a[2] + 2a[3]*t + 3a[4]*t^2 + 4a[5]*t^3 + 5a[6]*t^4
-end
-
-function acc_scale_at_t(a, t)
-    dds = 2a[3] + 6a[4]*t + 12a[5]*t^2 + 20a[6]*t^3
-end
-
-function get_path!(poses, vels, θ1, θ2, T, a, num_its=num_its)
-    dt = T/num_its
+function get_pose_list(pts, a, T, num_its=num_its)
+    dt = T/num_its 
+    poses = Array{Transform3D}(undef, num_its)
+    vels = Array{Transform3D}(undef, num_its)
 
     for i = 1:num_its
-        t = dt*i
-        s = pos_scale_at_t(a, t)
-        ds = vel_scale_at_t(a, t)
-        poses[i] = θ1 + s*(θ2-θ1)
-        vels[i] = ds*(θ2-θ1)
+        t_current = dt*i    # current time
+        s = TrajGen.s(t_current, a)
+        ds = TrajGen.ds(t_current, a)
+        poses[i] = get_T_at_s(pts, s)
+        display(poses[i].mat)
+        vels[i] = get_Tdot_at_sdot(pts, s, ds)
     end
     return poses, vels 
 end
 
+function find_trajectory(pts::worldSpaceWaypoints; T_init=1.0)
+    T = T_init
+    within_vel_bounds = false
+    a = Vector{Float64}(undef, 6)
+
+    # Increase trajectory duration until the maximum speed is not exceeded (or until it's longer than 15 seconds)
+    while within_vel_bounds == false && T < max_duration
+        max_lin_vels = 15/(8*T)*(translation(pts.end_pose)-translation(pts.start_pose))
+        if maximum(abs.(max_lin_vels)) > max_linear_vel
+            T = T + 0.2
+        else 
+            a = get_coeffs(T)
+            println(a)
+            within_vel_bounds = true
+        end
+    end
+    println(T)
+
+    # If a valid trajectory was found, get time series of poses and velocities
+    if within_vel_bounds == true 
+        poses, vels = get_pose_list(pts, a, T)
+        return [a, poses, vels]
+    else
+        println("No path between points; try again.")
+        return nothing
+    end
+
+end
+
+# ----------------------------------------------------------
+#               Trajectory Generation (Joint Space)
+# ----------------------------------------------------------
 """
     get_desv_at_t(t, p)
     
@@ -208,48 +234,6 @@ function scale_trajectory(params, poses, vels)
         (poses[:,i], vels[:,i]) = get_path!(poses[:,i], vels[:,i], params.wp.start.θs[i], params.wp.goal.θs[i], T, a[i,:])
     end
     return [trajParams(a, params.wp, T), poses, vels]
-end
-
-function find_trajectory(pts::Waypoints; num_its=num_its, T_init=1.0)
-    T = T_init
-    poses = Array{Float64}(undef, num_its, num_trajectory_dofs)
-    vels = Array{Float64}(undef, num_its, num_trajectory_dofs)
-    feasible_ct = 0
-    a = Array{Float64}(undef, num_trajectory_dofs, 6)
-
-    while feasible_ct < 4 && T < 10.0
-        feasible_ct = 0
-        for i in 1:num_trajectory_dofs
-            # println("Joint $(i)")
-            # Get trajectory 
-            a[i,:] = get_coeffs(pts, T, i)
-            (poses[:,i], vels[:,i]) = get_path!(poses[:,i], vels[:,i], pts.start.θs[i], pts.goal.θs[i], T, a[i,:])
-
-            # Evaluate if possible
-            is_in_range_poses = check_lim(poses[:,i], joint_lims, i)
-            is_in_range_vels = check_lim(vels[:,i], vel_lims, i)
-            # println("OK poses = $(is_in_range_poses), OK vels = $(is_in_range_vels)")
-            if is_in_range_poses == true && is_in_range_vels == true
-                feasible_ct = feasible_ct + 1
-            # else
-                # println("Max joint: $(maximum(poses[:,i])), min joint: $(minimum(poses[:,i]))")
-            end
-        end
-        if feasible_ct < num_trajectory_dofs
-            T = T + 0.2
-        end
-    end
-
-    if feasible_ct == num_trajectory_dofs
-        # println("Trajectory Parameters set")
-        # println("Poses: $(poses)")
-        return [trajParams(a, pts, T), poses, vels]
-        # println("Trajectory parameters set")
-    else
-        # println("No path between points; try again.")
-        return nothing
-    end
-
 end
 
 end
