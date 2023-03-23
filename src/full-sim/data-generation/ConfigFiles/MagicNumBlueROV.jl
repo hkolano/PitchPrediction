661f45f5c61bcefdb2fa899ad5effa1968fd72d0
Hannah Kolano <hkolano@comcast.net>
2023-03-23 13:56:21 -0700

    adding magic numbers

---
+++ src/full-sim/data-generation/ConfigFiles/MagicNumBlueROV.jl
@@ -0,0 +1,31 @@
+# Initialize dictionaries for buoyancy forces
+cob_vec_dict = Dict{String, SVector{3,Float64}}()
+cob_vec_dict["vehicle"] = SVector{3, Float64}([0.0, 0.0, 0.02])
+buoyancy_mag_dict = Dict{String, Float64}()
+buoyancy_mag_dict["vehicle"] = 10.23*9.81 #(volume * gravity)
+buoyancy_force_dict = Dict{String, FreeVector3D}()
+
+# Initialize dictionary for gravitational forces
+com_vec_dict = Dict{String, SVector{3, Float64}}()
+com_vec_dict["vehicle"] = SVector{3, Float64}([0.0, 0.0, 0.0])
+grav_mag_dict = Dict{String, Float64}()
+grav_mag_dict["vehicle"] = 10.0*9.81 #(weight * gravity)
+gravity_force_dict = Dict{String, FreeVector3D}()
+
+# Drag coefficients for the vehicle
+d_lin_coeffs = [4.03, 6.22, 5.18, .07, .07, .07]
+d_nonlin_coeffs = [18.18, 21.66, 36.99, 1.55, 1.55, 1.55]
+
+# Torque limits (from documentation, except jaw, which is a WAG)
+torque_lim_dict = Dict("roll" => 0.0,
+                        "pitch" => 0.0,
+                        "yaw" => 20., 
+                        "x" => 71.5,
+                        "y" => 88.2,
+                        "z" => 177.) 
+dtau_lim_dict = Dict("roll" => .001, 
+                    "pitch" => .001,
+                    "yaw" => .001,
+                    "x" => .001,
+                    "y" => .001,
+                    "z" => .001)
