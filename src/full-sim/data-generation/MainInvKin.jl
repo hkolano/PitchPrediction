a41b325d03938215b292ccb5dbb934c825b3adbe
Hannah Kolano <hkolano@comcast.net>
2023-04-05 12:52:04 -0700

    roll and pitch correct in kinematics sandbox

--- src/full-sim/data-generation/MainInvKin.jl
+++ src/full-sim/data-generation/MainInvKin.jl
@@ -35,9 +35,6 @@ urdf_file = joinpath("urdf", "blue_rov_revjaw.urdf")
 # ----------------------------------------------------------
 mech_blue_alpha, mvis, joint_dict, body_dict = mechanism_reference_setup(urdf_file)
 
-include("TrajGenWorld.jl")
-include("Tasks.jl")
-
 cob_frame_dict, com_frame_dict = setup_frames(body_dict, body_names, cob_vec_dict, com_vec_dict)
 buoyancy_force_dict, gravity_force_dict = setup_buoyancy_and_gravity(buoyancy_mag_dict, grav_mag_dict)
 
@@ -45,3 +42,6 @@ state = MechanismState(mech_blue_alpha)
 num_dofs = num_velocities(mech_blue_alpha)
 num_actuated_dofs = num_dofs-2
 println("Mechanism built.")
+
+include("TrajGenWorld.jl")
+include("Tasks.jl")
